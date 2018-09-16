6fb7538c0409f85ef4e6e2358205eed99de82894
KDyce <43318783+KDyce@users.noreply.github.com>
2018-09-16 11:32:54 -0400

    Initial commit

---
+++ Assets/Player_Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeath : MonoBehaviour {
+
+	public int heath;
+	public bool hasDied; 
+	
+	// Use this for initialization
+	void Start () {
+		 hasDied = false;
+	}
+	
+	void Update () {
+		if (gameObject.transform.position.y < -7) {
+			Debug.Log ("Player HAs Died");
+		}
+	}
+}
